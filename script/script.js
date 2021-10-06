52e03cb889db86ddbb95c5997082cccc4a32641a
ViggoOhlsson <viggo.e.g.o.@gmail.com>
2021-10-06 14:26:12 +0200

    dropdown funktionell

---
+++ script/script.js
@@ -0,0 +1,9 @@
+function toggleDropdown() {
+    var x = document.getElementById("dropDown");
+    if (x.style.visibility === "visible") {
+      x.style.visibility = "hidden";
+    } else {
+      x.style.visibility = "visible";
+    }
+  }
+  alert('js aktivt');
