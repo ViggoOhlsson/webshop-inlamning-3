2a5c6cc175a98775e85b2f26cf1ae4ec6c18f902
ViggoOhlsson <viggo.e.g.o@gmail.com>
2021-10-12 11:37:25 +0200

    header dropdown fixad

--- script/script.js
+++
@@ -1,8 +0,0 @@
-function toggleDropdown() {
-    var dropDown = document.getElementById("dropDown");
-    if (dropDown.style.visibility === "visible") {
-      dropDown.style.visibility = "hidden";
-    } else {
-      dropDown.style.visibility = "visible";
-    }
-  }
